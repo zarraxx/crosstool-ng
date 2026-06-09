0d594ad02b8b80991a95ff8bcac0d52ded6c8360
zarra <xsp@xyan.net.cn>
2026-06-09 11:18:16 +0000

    Add GCC release workflow

--- build.sh
+++ build.sh
@@ -9,12 +9,15 @@ DIST=$ROOT_DIR/dist
 mkdir -p $DIST
 mkdir -p $ARCHIVE_DIR
 
+ARCH=$(uname -m)
+TRIPLE=${TRIPLE:-$ARCH-unknown-linux-gnu}
+
 podman run --rm -it \
     --userns=keep-id \
-    -e LINES=50 -e COLUMNS=160 \
+    -e LINES=50 -e COLUMNS=160 -e TRIPLE=$TRIPLE \
     -v $ARCHIVE_DIR:/home/ctng/src \
     -v $ROOT_DIR/ctng_workspace:/home/ctng/workspace \
     -v $ROOT_DIR/container_script:/home/ctng/scripts \
     -v $DIST:/home/ctng/x-tools \
     $IMAGE \
-    bash #/home/ctng/scripts/build_toolchain.sh
+    bash /home/ctng/scripts/build_toolchain.sh
