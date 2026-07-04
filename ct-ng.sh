c0359d4bf4b4a1d6996fc3c114128ee7d51a52c9
zarra <xsp@xyan.net.cn>
2026-07-04 00:06:45 +0000

    统一增加 CT_TARGET_CFLAGS="-gdwarf-3"

---
+++ ct-ng.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+ROOT_DIR=$(dirname "$(readlink -f "$0")")
+
+IMAGE=ghcr.io/zarraxx/crosstool-ng:1.28.0
+ARCHIVE_DIR=$ROOT_DIR/archives
+DIST=$ROOT_DIR/dist
+mkdir -p $DIST
+mkdir -p $ARCHIVE_DIR
+
+ARCH=$(uname -m)
+TRIPLE=${TRIPLE:-$ARCH-unknown-linux-gnu}
+HOST=${HOST:-native}
+CTNG_ACTION=${CTNG_ACTION:-build}
+
+podman run --rm -it \
+    --userns=keep-id \
+    -e LINES=50 -e COLUMNS=160 -e TRIPLE=$TRIPLE -e HOST=$HOST \
+    -e CTNG_ACTION=$CTNG_ACTION \
+    -v $ARCHIVE_DIR:/home/ctng/src \
+    -v $ROOT_DIR/ctng_workspace:/home/ctng/workspace \
+    -v $ROOT_DIR/container_script:/home/ctng/scripts \
+    -v $DIST:/home/ctng/x-tools \
+    $IMAGE \
+    bash 
